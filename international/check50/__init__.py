3aa25337ce4a8032f8e28680ebf9dd5c915427c3
Benjamin Doran <benadoran@gmail.com>
2017-10-23 00:45:56 -0400

    simplified checks

--- international/check50/__init__.py
+++ international/check50/__init__.py
@@ -12,17 +12,11 @@ class International(Checks):
     @check("exists")
     def US_currency(self):
         """ on input 25 10 5 1 (.41) -> 4 """
-        expected = "you can make change with 4 coins\n"
-        actual = self.spawn("python international 25 10 5 1").stdout()
-        if not re.match(expected, actual):
-            err = Error(actual))
-            raise err
+        expected = "4\n"
+        actual = self.spawn("python international 25 10 5 1").stdout("^4\n", expected)
 
     @check("exists")
     def other(self):
         """ on input 18 5 7 1 (1.41) -> 10 """
-        expected = "you can make change with 10 coins\n"
-        actual = self.spawn("python international 18 5 7 1").stdout()
-        if not re.match(expected, actual):
-            err = Error(actual))
-            raise err
+        expected = "10\n"
+        actual = self.spawn("python international 18 5 7 1").stdout("^10\n", expected)
