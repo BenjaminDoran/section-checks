e7d1b8ddda8e99f7f746f9539d4bf0f062b729bb
Benjamin Doran <benadoran@gmail.com>
2017-10-23 00:58:48 -0400

    Expanded explainations

--- international/check50/__init__.py
+++ international/check50/__init__.py
@@ -11,7 +11,7 @@ class International(Checks):
 
     @check("exists")
     def US_currency(self):
-        """ on input 25 10 5 1 (.41) -> 4 """
+        """ on input .41 with denominations 25 10 5 1 yields 4 """
         expected = "4\n"
         actual = self.spawn("python international 25 10 5 1")\
                      .stdin('.41')\
@@ -19,7 +19,7 @@ class International(Checks):
 
     @check("exists")
     def other(self):
-        """ on input 18 5 7 1 (1.41) -> 10 """
+        """ on input 1.41 with denominations 18 5 7 1 yields 10 """
         expected = "10\n"
         actual = self.spawn("python international 18 5 7 1")\
                      .stdin('1.41')\
