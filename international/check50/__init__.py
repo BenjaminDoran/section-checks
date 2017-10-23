63931e63b3e4560d36d7fc4657c2fd2b1ee28ba8
Benjamin Doran <benadoran@gmail.com>
2017-10-23 00:55:14 -0400

    added standard input

--- international/check50/__init__.py
+++ international/check50/__init__.py
@@ -13,10 +13,14 @@ class International(Checks):
     def US_currency(self):
         """ on input 25 10 5 1 (.41) -> 4 """
         expected = "4\n"
-        actual = self.spawn("python international 25 10 5 1").stdout("^4\n", expected)
+        actual = self.spawn("python international 25 10 5 1")\
+                     .stdin('.41')\
+                     .stdout("^4\n", expected)
 
     @check("exists")
     def other(self):
         """ on input 18 5 7 1 (1.41) -> 10 """
         expected = "10\n"
-        actual = self.spawn("python international 18 5 7 1").stdout("^10\n", expected)
+        actual = self.spawn("python international 18 5 7 1")\
+                     .stdin('1.41')\
+                     .stdout("^10\n", expected)
